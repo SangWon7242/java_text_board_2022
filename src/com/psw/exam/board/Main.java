1764d4946c9a81f8264af8ded853fc07ced59e25
SangWon7242 <gotkdqja@gmail.com>
2022-05-21 14:17:17 +0900

    테스트 게시물 생성 함수 적용

--- src/com/psw/exam/board/Main.java
+++ src/com/psw/exam/board/Main.java
@@ -4,75 +4,75 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		
-		System.out.println("== 게시판 v 0.1 == ");
-		System.out.println("== 프로그램 시작 == ");
-		
-		int articleLastId = 0;
-		Article lastArticle = null;
-		
-		ArrayList<Article> articles = new ArrayList<Article>(); 
-		
-		// 테스트 데이터 3개 등록 시작
+	static void makeTestData(ArrayList<Article> articles) {
 		articles.add(new Article(1, "제목1", "내용1"));
 		articles.add(new Article(2, "제목2", "내용2"));
 		articles.add(new Article(3, "제목3", "내용3"));
-		// 테스트 데이터 3개 등록 끝
-		
-		while ( true ) {
+	}
+	
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("== 게시판 v 0.1 == ");
+		System.out.println("== 프로그램 시작 == ");
+
+		int articleLastId = 0;
+		Article lastArticle = null;
+
+		ArrayList<Article> articles = new ArrayList<Article>();
+
+		makeTestData(articles);
+
+		while (true) {
 			System.out.printf("명령) ");
 			String cmd = sc.nextLine();
-			
-			if ( cmd.equals("exit")) {
+
+			if (cmd.equals("exit")) {
 				break;
-			}
-			else if ( cmd.equals("/usr/article/list")) {
+			} else if (cmd.equals("/usr/article/list")) {
 				System.out.println("- 게시물 리스트 -");
 				System.out.println("-----------------");
 				System.out.println("번호 / 제목");
 				System.out.println("-----------------");
-				
-				for( Article article : articles ) {
+
+				for (Article article : articles) {
 					System.out.printf("%d / %s\n", article.id, article.title);
 				}
-				
-			}
-			else if ( cmd.equals("/usr/article/detail")) {
+
+			} else if (cmd.equals("/usr/article/detail")) {
 				Article article = lastArticle;
-				
-				if ( lastArticle == null ) {
+
+				if (lastArticle == null) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-				
+
 				System.out.println("- 게시물 상세보기 -");
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
 				System.out.printf("내용 : %s\n", article.body);
-			}
-			else if ( cmd.equals("/usr/article/write")) {
+			} else if (cmd.equals("/usr/article/write")) {
 				System.out.println("- 게시물 등록 - ");
 				System.out.printf("제목: ");
 				String title = sc.nextLine();
 				System.out.printf("내용: ");
 				String body = sc.nextLine();
-				
+
 				int id = articleLastId + 1;
 				articleLastId = id;
-				
+
 				Article article = new Article(id, title, body);
 				lastArticle = article;
-			
+
 				System.out.println("입력된 객체 : " + article);
-				
+
 				System.out.printf("%d번 게시물이 입력되었습니다.\n", id);
 			}
-			
+
 			System.out.printf("입력된 명령어 : %s\n", cmd);
 		}
-		
+
 		System.out.println("== 프로그램 종료 == ");
 		sc.close();
 	}
@@ -82,8 +82,7 @@ class Article {
 	int id;
 	String title;
 	String body;
-	
-	
+
 	public Article(int id, String title, String body) {
 		this.id = id;
 		this.title = title;
@@ -94,5 +93,5 @@ class Article {
 	public String toString() {
 		return String.format("{id: %d, title: %s, body: %s}", id, title, body);
 	}
-	
+
 }
