c6b1c799864f4e6f6997f275c3d229165f3bc28a
SangWon7242 <gotkdqja@gmail.com>
2022-06-04 12:45:53 +0900

    게시물 상세보기 시에, 원하는 게시물 번호 입력

--- src/com/psw/exam/board/Main.java
+++ src/com/psw/exam/board/Main.java
@@ -33,6 +33,7 @@ public class Main {
 			String cmd = sc.nextLine();
 			
 			Rq rq = new Rq(cmd);
+			Map<String, String> params = rq.getParams();
 
 			if (rq.getUrlPath().equals("exit")) {
 				break;
@@ -48,14 +49,22 @@ public class Main {
 				}
 
 			} else if (rq.getUrlPath().equals("/usr/article/detail")) {
-
+				
+				int id = Integer.parseInt(params.get("id"));
+						
+				
 				if (articles.isEmpty()) {
 					System.out.println("게시물이 존재하지 않습니다.");
 					continue;
 				}
-
-				Article article = articles.get(articles.size() - 1);
-
+				
+				Article article = articles.get(id - 1);
+				
+				if ( id > articles.size() ) {
+					System.out.println("게시물이 존재하지 않습니다.");
+					continue;
+				}
+				
 				System.out.println("- 게시물 상세보기 -");
 				System.out.printf("번호 : %d\n", article.id);
 				System.out.printf("제목 : %s\n", article.title);
